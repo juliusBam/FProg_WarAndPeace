6a985a67ffd0891f97165871a45a5202cf588e52
juliob <juliobampi97@gmail.com>
2024-01-11 15:16:22 +0100

    Content grouped by chapter and divided as words

--- FProg_WarAndPeace_GruppeG/Program.fs
+++ FProg_WarAndPeace_GruppeG/Program.fs
@@ -1,5 +1,6 @@
 ﻿open System.IO
 open WarAndPeaceFProg.FileOperations
+open WarAndPeaceFProg.ContentParsing
 
 [<EntryPoint>]
 let main (args: string array) =
@@ -7,6 +8,8 @@ let main (args: string array) =
     let bookStartFlag = "CHAPTER 1"
     let bookEndFlag = " END OF THE PROJECT GUTENBERG EBOOK WAR AND PEACE "
     let filteredContent = truncateAfterMatch (truncateUntilMatch content bookStartFlag) bookEndFlag
+    let splitContent = partitionContentByChapter filteredContent
+    let splitWordsContent = splitLines splitContent
     printfn $"%A{filteredContent}"
     printfn $"%A{filteredContent |> Seq.last}"
     0
--- FProg_WarAndPeace_GruppeG/WarAndPeaceFProg/ContentParsing.fs
+++ FProg_WarAndPeace_GruppeG/WarAndPeaceFProg/ContentParsing.fs
@@ -1,14 +1,43 @@
 namespace WarAndPeaceFProg
 
+open System.Security.Cryptography
+
 module ContentParsing =
     //separate into chapters
     let isLineNewChapter (actualLine: string) =
         let lineParts = actualLine.Split " "
         lineParts.Length = 2 && lineParts[0].ToLower().Equals "chapter"
         
+    let splitByChapter (accumulationState: string list list) (line: string) =
+        if isLineNewChapter line then
+            [] :: accumulationState //for a new chapter we insert an empty list
+        else
+            match accumulationState with
+            | [] -> [[line]] //if actual accumulationState is empty just add the new string
+            | currentChapter :: last -> (line :: currentChapter) :: last
+            //| [] -> [line.Split([|' '|]) |> Array.toList |> List.rev] //if actual accumulationState is empty just add the new string
+            //| currentChapter :: rest -> line.Split([|' '|]) |> Array.toList |> List.rev @ currentChapter :: rest
+            
+    let splitSingleLine (chapterContent: string list) =
+        chapterContent |> List.map (fun line -> line.Split(" ") |> Array.toList) |> List.concat
+            
+    let splitLines (partitionedContent: string list list) =
+        partitionedContent |> List.map splitSingleLine
         
-    //todo separate in chars
-    let separateIntoChapters (sequence: seq<string>) =
-             let chaptersSeq: seq<seq<string>> = []
-             let a = sequence |> Seq.takeWhile (fun line -> not (isLineNewChapter line)) |> Seq.skip 1
-             chaptersSeq
+    let partitionContentByChapter (content: string seq) =
+        content |> Seq.fold splitByChapter [] |> List.map(fun element -> element |> List.rev) |> List.rev
+        
+    // let result =
+    //     inputSequence
+    //     |> Seq.fold splitByChapter []
+    //     |> List.rev
+
+        
+        
+    // //todo separate in chars
+    // let separateIntoChapters (sequence: seq<string>) =
+    //          let chaptersSeq: seq<seq<string>> = []
+    //          let a = sequence |> Seq.takeWhile (fun line -> not (isLineNewChapter line)) |> Seq.skip 1
+    //          chaptersSeq
+             
+    
