07813e6b68145fd42d28ac91d00ea454965d39b3
juliob <juliobampi97@gmail.com>
2024-01-16 14:03:45 +0100

    Working performant solution. Result is written in a file. At the moment 73 chapters are "wrong" in comparison with the uploaded solution

--- FProg_WarAndPeace_GruppeG/Program.fs
+++ FProg_WarAndPeace_GruppeG/Program.fs
@@ -8,19 +8,24 @@ let getBookContent =
     let bookEndFlag = " END OF THE PROJECT GUTENBERG EBOOK WAR AND PEACE "
     extractFormattedFileContent (extractFileContent bookStartFlag bookEndFlag)
     
-let printResult (chapterScores: int list) =
-        chapterScores |> List.indexed |> List.iter (fun el ->
+let mapResults (chapterScores: int list) =
+        chapterScores |> List.indexed |> List.map (fun el ->
                                                     let relationValue = snd el
                                                     match relationValue with
-                                                    | 0 -> printfn $"Chapter %A{fst el + 1}: no idea"
-                                                    | relationValue when relationValue > 0 -> printfn $"Chapter %A{fst el + 1}: war-related"
-                                                    | relationValue when relationValue < 0 -> printfn $"Chapter %A{fst el + 1}: peace-related"
+                                                    | 0 -> $"Chapter %A{fst el + 1}: no idea"
+                                                    | relationValue when relationValue > 0 -> $"Chapter %A{fst el + 1}: war-related"
+                                                    | relationValue when relationValue < 0 -> $"Chapter %A{fst el + 1}: peace-related"
                                                 )
+
+let writeStringsToFile (filePath: string) (content: seq<string>) =
+    use writer = new StreamWriter(filePath, false) // false for overwriting existing content
+    content |> Seq.iter (fun line -> writer.WriteLine(line))
 
 [<EntryPoint>]
 let main (args: string array) =
     let formattedBookContent = getBookContent
     let peaceTerms = Set.ofSeq (readFileContent "../../../peaceTerms.txt") //terms are saved in Sequence (like a Map but without values) for fast search operations O(log n) vs 0(n)
     let warTerms = Set.ofSeq (readFileContent "../../../warTerms.txt")
-    calculateChapterArgument formattedBookContent warTerms peaceTerms |> printResult
+    let result = calculateChapterArgument formattedBookContent warTerms peaceTerms |> mapResults |> Seq.ofList
+    writeStringsToFile "../../../output.txt" result
     0
--- FProg_WarAndPeace_GruppeG/WarAndPeaceFProg/ChapterAnalysis.fs
+++ FProg_WarAndPeace_GruppeG/WarAndPeaceFProg/ChapterAnalysis.fs
@@ -5,23 +5,40 @@ module ChapterAnalysis =
         if (warTerms |> Set.contains word) then 1
         else if (peaceTerms |> Set.contains word) then -1
         else 0
-
-    let curriedAnalyseWords warTerms peaceTerms chapterContent =
+    
+    let analyseChapterContent warTerms peaceTerms chapterContent =
         let applyFindWordMatch = curriedFindWordMatch warTerms peaceTerms
         chapterContent |> List.map applyFindWordMatch |> List.sum
-
-    let calculateChapterArgument (formattedBookContent: string list list) (warTerms: string Set) (peaceTerms: string Set) =
-        let analyseChapterContent = curriedAnalyseWords warTerms peaceTerms
-        formattedBookContent |> List.map analyseChapterContent
-
-    let indexWithFilter (warTerms: Set<string>)  (chapterContent: string list) = 
+        
+    let indexWithFilter (termsToMatch: Set<string>)  (chapterContent: string list) = 
         chapterContent 
-        |> List.indexed 
+        |> List.indexed
         |> List.filter (
-            fun (index: int, value: string) -> warTerms |> Set.contains value)
+            fun (_: int, value: string) -> termsToMatch |> Set.contains value)
         |> List.map fst // fst returns first element of (index, value) tuple
+    
     let calculateDistance (indexList: int List) = 
         indexList 
         |> List.pairwise
-        |> List.map (
-            fun (first: int, second: int) -> second - first) 
+        |> List.map (fun (first: int, second: int) -> second - first)
+    let calculateDensity (distanceList: int list) =
+         //73 differences to the reference
+         let distanceSum = double (distanceList |> List.sum)
+         if (distanceSum = 0) then double 0 else double (distanceList.Length + 1) / distanceSum
+         //85 differences to the reference
+         //double (distanceList |> List.sum) / double (distanceList.Length + 1)
+         //84 differences to the reference
+         //double (distanceList.Length + 1) / double (distanceList |> List.sum)
+         //84 differences to the reference
+         //if distanceList.IsEmpty then double 0 else distanceList |> List.averageBy double
+        
+    let parseChapterReturnTopic (warTerms: string Set) (peaceTerms: string Set) (acc: int list) (chapterContent: string list) =
+        if (calculateDensity (calculateDistance (indexWithFilter peaceTerms chapterContent)) >
+                calculateDensity (calculateDistance (indexWithFilter warTerms chapterContent))) then -1 :: acc //faster then adding at the end of the list --> have to revert
+        else 1 :: acc
+    
+    let calculateChapterArgument (formattedBookContent: string list list) (warTerms: string Set) (peaceTerms: string Set) =
+        let curriedParseChapterReturnTopic = parseChapterReturnTopic warTerms peaceTerms
+        List.fold curriedParseChapterReturnTopic [] formattedBookContent |> List.rev //we use list rev because it is faster to append the elements at the beginning of the list
+        //and then invert it, List.fold is already parallel
+       
