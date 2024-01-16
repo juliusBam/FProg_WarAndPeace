bad247f82bc0e66919435d04c0973d195ed3af40
juliob <juliobampi97@gmail.com>
2024-01-16 14:27:24 +0100

    Improved results, only 55 "wrong" in comparison to the reference solution. However method to calculate density is strange

--- FProg_WarAndPeace_GruppeG/WarAndPeaceFProg/ChapterAnalysis.fs
+++ FProg_WarAndPeace_GruppeG/WarAndPeaceFProg/ChapterAnalysis.fs
@@ -21,10 +21,16 @@ module ChapterAnalysis =
         indexList 
         |> List.pairwise
         |> List.map (fun (first: int, second: int) -> second - first)
+
     let calculateDensity (distanceList: int list) =
+        //55 differences to the reference
+        if (distanceList.IsEmpty) then double 0 else double (distanceList.Length + 1) / (distanceList |> List.averageBy double) //we calculate the density as #matches/avg distance
          //73 differences to the reference
-         let distanceSum = double (distanceList |> List.sum)
-         if (distanceSum = 0) then double 0 else double (distanceList.Length + 1) / distanceSum
+         // let distanceSum = double (distanceList |> List.sum)
+         // if (distanceSum = 0) then double 0 else double (distanceList.Length + 1) / distanceSum
+        //67 differences to reference
+        // let distanceSum = double (distanceList |> List.sum)
+        // if (distanceSum = 0) then double 0 else chapterLength / (distanceList |> List.averageBy double)
          //85 differences to the reference
          //double (distanceList |> List.sum) / double (distanceList.Length + 1)
          //84 differences to the reference
@@ -33,9 +39,10 @@ module ChapterAnalysis =
          //if distanceList.IsEmpty then double 0 else distanceList |> List.averageBy double
         
     let parseChapterReturnTopic (warTerms: string Set) (peaceTerms: string Set) (acc: int list) (chapterContent: string list) =
-        if (calculateDensity (calculateDistance (indexWithFilter peaceTerms chapterContent)) >
-                calculateDensity (calculateDistance (indexWithFilter warTerms chapterContent))) then -1 :: acc //faster then adding at the end of the list --> have to revert
+        if ((calculateDensity (calculateDistance (indexWithFilter peaceTerms chapterContent)))) >
+                (calculateDensity (calculateDistance (indexWithFilter warTerms chapterContent))) then -1 :: acc //faster then adding at the end of the list --> have to revert
         else 1 :: acc
+       
     
     let calculateChapterArgument (formattedBookContent: string list list) (warTerms: string Set) (peaceTerms: string Set) =
         let curriedParseChapterReturnTopic = parseChapterReturnTopic warTerms peaceTerms
