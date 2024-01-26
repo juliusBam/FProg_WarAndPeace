c9d4b57fcb1774eadbe1d988eb3fc4733a0333f2
PoliteCupcake <sbieg96n@gmail.com>
2024-01-26 04:14:28 +0100

    Added startup
    Consider differnt file structure!

---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set environment variables for file paths 
+export BOOK_PATH="./Resources/book.txt"
+export WAR_TERMS="./Resources/warTerms.txt"
+export PEACE_TERMS="./Resources/peaceTerms.txt"
+export OUTPUT_FILE="./Resources/output.txt"
+
+./FProg_WarAndPeace_GruppeG/bin/Debug/net8.0/FProg_WarAndPeace_GruppeG
