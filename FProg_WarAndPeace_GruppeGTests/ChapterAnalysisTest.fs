484576fcf70a800115198a66144787ac80d64400
juliob <juliobampi97@gmail.com>
2024-01-27 20:59:02 +0100

    Added unit testing to project. Used generator when possible for property testing

---
+++ FProg_WarAndPeace_GruppeGTests/ChapterAnalysisTest.fs
@@ -0,0 +1,101 @@
+module FProg_WarAndPeace_GruppeGTests.ChapterAnalysisTest
+
+open System
+open NUnit.Framework
+open FsCheck.FSharp
+open WarAndPeaceFProg.ChapterAnalysis
+
+//no empty spaces to simulate single words
+let generateRandomWord () =
+    let rnd = Random()
+    let length = rnd.Next(40, 200)
+    let alphanumericChars = ['a'..'z'] @ ['A'..'Z'] @ ['0'..'9']
+    let getChar () = alphanumericChars.[rnd.Next(alphanumericChars.Length)]
+    let randomString = String([|for _ in 1..length -> getChar ()|])
+    randomString
+
+//takes a random "match" from the specified list
+let takeRandomElFromList (list: string list) =
+    let rnd = Random()
+    let index = rnd.Next(0, list.Length) //rnd.Next upper bound is exclusive
+    list[index]
+
+//takes x random elements from the specified list
+let takeFromList (list: string list) (numberOfTimes: int) =
+    List.init numberOfTimes (fun _ -> takeRandomElFromList list)
+    
+//returns concatenation of matches for A, random words and matches for B
+//no shuffling, no decent library found including it, implementation from
+//scratch would not introduce desired randomness
+let mixListsGenerator numberOfElementsA elementsA numberOfElementsB elementsB =
+    Gen.choose (numberOfElementsA + numberOfElementsB + 1, 5 * (numberOfElementsA + numberOfElementsB) ) // Change the range as needed for the random number of words
+    |> Gen.map (fun randomCount ->
+        let words = List.init randomCount (fun _ -> generateRandomWord ())
+        let elementsA = takeFromList elementsA numberOfElementsA
+        let elementsB = takeFromList elementsB numberOfElementsB
+        elementsA @ words @ elementsB 
+    )
+
+let numberOfMatchesProperty (matchesA: int) (listMatchesTopicA: string list) (matchesB: int) (listMatchesTopicB: string list) (words: string list) =
+    let calculatedNumberOfMatchesA = (indexOfMatchesWithSet (Set.ofList listMatchesTopicA) words).Length
+    let calculatedNumberOfMatchesB = (indexOfMatchesWithSet (Set.ofList listMatchesTopicB) words).Length
+    calculatedNumberOfMatchesA = matchesA && calculatedNumberOfMatchesB = matchesB
+
+//test that the counted number of matches are right
+[<Test>]
+let testNumberOfMatches () =
+    
+    let listMatchesTopicA = ["MATCHING-";"MAKES-";"FUN-"]
+    
+    let listMatchesTopicB = ["ICE-";"CREAM-";"GOOD-"]
+    
+    let matchesA = 5
+    let matchesB = 10
+    let partiallyAppliedGen = mixListsGenerator matchesA listMatchesTopicA matchesB listMatchesTopicB
+    let text = Gen.sample 10 partiallyAppliedGen |> Array.toList
+    
+    let partiallyAppliedProperty = numberOfMatchesProperty matchesA listMatchesTopicA matchesB listMatchesTopicB
+    let result = text |> List.forall partiallyAppliedProperty
+    
+    Assert.True(result)
+    
+let densityNumberOfMatchesProp (matchesA: int) (listMatchesTopicA: string list) (matchesB: int) (listMatchesTopicB: string list) (words: string list) =
+    let calculatedMatchesA = (indexOfMatchesWithSet (Set.ofList listMatchesTopicA) words)
+    let calculatedMatchesB = (indexOfMatchesWithSet (Set.ofList listMatchesTopicB) words)
+    
+    let calculatedDensityA = calculateDensityNumberOfMatches words.Length calculatedMatchesA
+    let calculatedDensityB = calculateDensityNumberOfMatches words.Length calculatedMatchesB
+    
+    matchesA = (int)calculatedDensityA && matchesB = (int)calculatedDensityB 
+    
+let compareDensityCorrectProp (matchesA: int) (listMatchesTopicA: string list) (matchesB: int) (listMatchesTopicB: string list) (words: string list) =
+    
+    let calculatedMatchesA = (indexOfMatchesWithSet (Set.ofList listMatchesTopicA) words)
+    let calculatedMatchesB = (indexOfMatchesWithSet (Set.ofList listMatchesTopicB) words)
+    
+    let comparedDensity = compareDensities words.Length calculatedMatchesA calculatedMatchesB calculateDensityNumberOfMatches
+    
+    matchesA > matchesB = comparedDensity
+
+//test that the density as number of matches corresponds to the number of matches
+//test that the recognized argument is right
+[<Test>]
+let densityNumberOfMatches () =
+    
+    let listMatchesTopicA = ["MATCHING-";"MAKES-";"FUN-"]
+    let listMatchesTopicB = ["ICE-";"CREAM-";"GOOD-"]
+    
+    let matchesA = 20
+    let matchesB = 5
+    let partiallyAppliedGen = mixListsGenerator matchesA listMatchesTopicA matchesB listMatchesTopicB
+    let text = Gen.sample 10 partiallyAppliedGen |> Array.toList
+    
+    let partiallyDensityNumberOfMatchesProp = densityNumberOfMatchesProp matchesA listMatchesTopicA matchesB listMatchesTopicB
+    let resDensityValueCorrect = text |> List.forall partiallyDensityNumberOfMatchesProp
+    
+    Assert.True(resDensityValueCorrect)
+    
+    let partiallyCompareDensityCorrectProp = densityNumberOfMatchesProp matchesA listMatchesTopicA matchesB listMatchesTopicB
+    let relatedTopicRes = text |> List.forall partiallyCompareDensityCorrectProp
+    
+    Assert.True(relatedTopicRes)
---
+++ FProg_WarAndPeace_GruppeGTests/ContentParsingTests.fs
@@ -0,0 +1,135 @@
+module FProg_WarAndPeace_GruppeGTests.ContentParsingTests
+
+open FsCheck.FSharp
+open NUnit.Framework
+open System
+open WarAndPeaceFProg.ContentParsing
+open WarAndPeaceFProg.FileOperations
+
+let generateRandomLine () =
+    let rnd = Random()
+    let length = rnd.Next(40, 200)
+    let alphanumericChars = ['a'..'z'] @ ['A'..'Z'] @ ['0'..'9']
+    let getChar () =
+        if (rnd.Next(1, 8) < 2) then //force adding spaces to simulate words
+            ' '
+        else
+            alphanumericChars.[rnd.Next(alphanumericChars.Length)]
+    let randomString = String([|for _ in 1..length -> getChar ()|])
+    randomString
+
+//hardcoded words inserted for properties
+let firstWord = "FIRST"
+let secondWord = "WORDS"
+let truncateAfterMatchString = "*MYMATCH*"
+
+let generateListOfChapterContent =
+    Gen.choose(30, 50)
+    |> Gen.bind (fun lineNumb ->
+        Gen.elements [1..lineNumb]
+        |> Gen.map (fun i -> //First words are hardcoded to test if order is right, truncateaftermatchstring as well, so that we have a unique string (not only alphanumeric)
+            [String.Concat(firstWord, " ", secondWord)] @ List.init i (fun _ -> generateRandomLine()) @ [truncateAfterMatchString] @ List.init i (fun _ -> generateRandomLine()) 
+        )
+    )
+
+
+//Hard to do with property based testing --> would have to store results
+[<Test>]
+let chapterSplitting () =
+    
+    let firstLineChapter1 = "first line chapter 1"
+    let firstLineChapter2 = "first line chapter 2"
+    
+    let bookContent = ["CHAPTER 1"; firstLineChapter1; "second line chapter 1"; "CHAPTER 2"; firstLineChapter2; "second line chapter 2"]
+    
+    let result = partitionContentByChapter bookContent
+    
+    //book content not yet reverted
+    Assert.AreEqual(firstLineChapter1, result[0][0])
+    Assert.AreEqual(firstLineChapter2, result[1][0])
+    
+[<Test>]
+let lineOnlyChapterNoChapter () =
+    let isChapter = isLineNewChapter "Chapter"
+    Assert.False(isChapter)
+    
+[<Test>]
+let validLineIsChapter () =
+    let isChapter = isLineNewChapter "CHAPTER 2"
+    Assert.True(isChapter)
+
+//Test chapter lines correctly split
+[<Test>]
+let splitChapterIntoWords () =
+    let chapterContent = ["This is a line, of words"; "This is 89 bla: line, of words"]
+    let result = splitSingleLine chapterContent
+    
+    //Test words are present
+    Assert.AreEqual("This", result[0])
+    Assert.AreEqual("line,", result[3])
+    Assert.AreEqual("89", result[8])
+    Assert.AreEqual("bla:", result[9])
+    //Test length is right
+    Assert.AreEqual(13, result.Length)
+
+let areEmptySpacesRemoved (chapterContent: string list) =
+    chapterContent
+    |> List.forall
+           (fun s ->
+                not (s.Contains ' ')
+           )
+
+let areFirstWordsFirstAndWords (chapterContent: string list) =
+    chapterContent[0] = "FIRST" && chapterContent[1] = "WORDS"
+
+//Chapter words not containing empty spaces and size remaining the same
+//As well as order not changed
+[<Test>]
+let testSplittingOfGeneratedWords () =
+    let numbOfChapters = 5
+    let splittedChapters = splitLinesIntoWords ((Gen.sample numbOfChapters generateListOfChapterContent) |> Array.toList)
+    let noEmptySpaces = List.forall areEmptySpacesRemoved splittedChapters
+    let correctFirstWords = List.forall areFirstWordsFirstAndWords splittedChapters
+    
+    Assert.True(noEmptySpaces)
+    Assert.True(correctFirstWords)
+    Assert.AreEqual(numbOfChapters, splittedChapters.Length)
+
+let truncateUntilMatchIncludesMatchProperty (random: Random) (chapterContent: string list) =
+    if (not chapterContent.IsEmpty) then
+        let indexOfMatch = random.Next(0, chapterContent.Length - 1)
+        let valueOfMatch = chapterContent[indexOfMatch]
+        let truncatedList = truncateUntilMatch (chapterContent |> List.toSeq) valueOfMatch |> Seq.toList
+        truncatedList[0] = valueOfMatch
+    else
+        true
+        
+//avoid instantiating random for every try
+let partiallyAppliedTruncateUntilMatchIncludesMatchProperty (chapterContent: string list) =
+    let random = Random()
+    truncateUntilMatchIncludesMatchProperty random chapterContent
+
+//Usage of truncate untilMatch does not remove the match from the list
+[<Test>]
+let testTruncateUntilMatchIncludesMatch () =
+    let numbOfChapters = 5
+    let splittedChapters = splitLinesIntoWords ((Gen.sample numbOfChapters generateListOfChapterContent) |> Array.toList)
+    let matchIncluded = splittedChapters |> List.forall partiallyAppliedTruncateUntilMatchIncludesMatchProperty
+    
+    Assert.True(matchIncluded)
+
+let truncateAfterMatchDoesNotIncludeMatchProperty (chapterContent: string list) =
+    if (not chapterContent.IsEmpty) then
+        let truncatedList = truncateAfterMatch (chapterContent |> List.toSeq) truncateAfterMatchString
+        not (truncatedList |> Seq.contains (truncateAfterMatchString))
+    else
+        true
+
+//Usage of truncate afterMatch removes the match from the list
+[<Test>]
+let testTruncateAfterMatchNoIncludesMatch () =
+    let numbOfChapters = 5
+    let splittedChapters = splitLinesIntoWords ((Gen.sample numbOfChapters generateListOfChapterContent) |> Array.toList)
+    let matchNotPresent = splittedChapters |> List.forall truncateAfterMatchDoesNotIncludeMatchProperty 
+    
+    Assert.True(matchNotPresent)
