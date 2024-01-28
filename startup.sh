0257c840c128064530485b177bbd378d2dee59bc
juliob <juliobampi97@gmail.com>
2024-01-28 15:46:20 +0100

    Updated readme.md and startup script.

--- startup.sh
+++ startup.sh
@@ -5,14 +5,27 @@ export BOOK_PATH="./Resources/book.txt"
 export WAR_TERMS="./Resources/warTerms.txt"
 export PEACE_TERMS="./Resources/peaceTerms.txt"
 export OUTPUT_FILE="./output.txt"
+export RUNNABLE_PATH="./FProg_WarAndPeace_GruppeG/bin/Release/net8.0/FProg_WarAndPeace_GruppeG"
 
 # Compile project --> needed if compiled project does not work
-# requires install .NET 8 SDK and dotnet in $PATH variable
-#dotnet restore
-#dotnet build
+# requires installation of .NET 8 SDK and dotnet in $PATH variable
+if [ "$1" = "--build" ] || [ "$1" = "-b" ]; then
+    echo "The project's solution will be built"
+    dotnet restore
+    dotnet build -c Release
+    dotnet test
+else
+    echo "The project solution will not be rebuilt, using present binaries instead"
+fi
+
+#Check if binaries exists
+if [ ! -f "$RUNNABLE_PATH" ]; then
+    echo "Runnable file not found"
+    exit 1
+fi
 
 # Execute project
-./FProg_WarAndPeace_GruppeG/bin/Release/net8.0/FProg_WarAndPeace_GruppeG
+$RUNNABLE_PATH
 
 reference="./Resources/reference.txt" 
 
